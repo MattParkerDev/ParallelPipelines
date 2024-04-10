b11550cb504e96303770edd37b180cbd3a78655a
Matthew Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2024-04-10 22:29:41 +1000

    add stuff

---
+++ src/ModularPipelines.Host/Helpers/PipelineContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ModularPipelines.Host.Helpers;
+
+public class PipelineContext : IPipelineContext
+{
+	public IConfiguration Configuration { get; }
+}
+
+public interface IPipelineContext
+{
+	public IConfiguration Configuration { get; }
+}
