230e3a57c3c2d99660d82f9df52b57dfb5368390
Matthew Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2024-04-11 23:45:52 +1000

    Create PipelineFileHelper.cs

---
+++ src/ModularPipelines.Host/Helpers/PipelineFileHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.IO.Compression;
+using Microsoft.Extensions.FileSystemGlobbing.Abstractions;
+
+namespace ModularPipelines.Host.Helpers;
+
+public static class PipelineFileHelper
+{
+	public static Task<DirectoryInfo> GetDirectory(string directoryPath)
+	{
+		var directoryInfo = new DirectoryInfo(directoryPath);
+		return Task.FromResult(directoryInfo);
+	}
+
+	public static Task<FileInfo> GetFile(string targetFilePath)
+	{
+		var fileInfo = new FileInfo(targetFilePath);
+		return Task.FromResult(fileInfo);
+	}
+
+	public static async Task WriteTextToFileAsync(string targetFilePath, string content)
+	{
+		await File.WriteAllTextAsync(targetFilePath, content);
+	}
+
+	public static async Task<string> ReadTextFromFileAsync(string targetFilePath)
+	{
+		return await File.ReadAllTextAsync(targetFilePath);
+	}
+
+	public static async Task ZipDirectory(string sourceDirectory, string targetFilePath)
+	{
+		ZipFile.CreateFromDirectory(sourceDirectory, targetFilePath);
+	}
+}
+
+// public class FileInfo
+// {
+// 	public string Name { get; set; }
+// 	public string FullName { get; set; }
+// 	public string Extension { get; set; }
+// 	public long Length { get; set; }
+// 	public DateTime LastWriteTime { get; set; }
+// }
